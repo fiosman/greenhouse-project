e36fd85297c661df107ab41d4bad81a301ef5431
fiosman <fares.osman@outlook.com>
2022-01-21 23:47:01 -0500

    Add import statements for context

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,18 +1,16 @@
-import dayImage from './images/greenhouse-day.jpg';
-import nightImage from './images/greenhouse-night.jpg';
-import './Greenhouse.css';
-
-import LightSwitch from './LightSwitch';
-import ClimateStats from './ClimateStats';
+import dayImage from "./images/greenhouse-day.jpg";
+import nightImage from "./images/greenhouse-night.jpg";
+import "./Greenhouse.css";
+import LightSwitch from "./LightSwitch";
+import ClimateStats from "./ClimateStats";
+import { useTheme } from "../../context/ThemeContext";
 
 function Greenhouse() {
+  const [themeName, setThemeName] = useTheme();
 
   return (
     <section>
-      <img  className='greenhouse-img'
-            src={dayImage}
-            alt='greenhouse' 
-      />
+      <img className="greenhouse-img" src={dayImage} alt="greenhouse" />
       <LightSwitch />
       <ClimateStats />
     </section>
