bfd8bc6f7327720ee51dad60415d25ecc5d2768c
fiosman <fares.osman@outlook.com>
2022-01-24 23:26:44 -0500

    Finish Phase Two

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -10,8 +10,8 @@ export default function ThemeProvider({ children }) {
   return (
     <ThemeContext.Provider
       value={{
-        themeName: "night",
-        setThemeName,
+        themeName: themeName,
+        setThemeName: setThemeName,
       }}
     >
       {children}
