7c9d5ce89e9be570e43ed5dd127002d7a98d8d01
fiosman <fares.osman@outlook.com>
2022-01-25 23:58:18 -0500

    Creates climate context

--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -1,2 +1,6 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+
+import React, { useContext, createContext } from "react";
+
+const ClimateContext = createContext();
