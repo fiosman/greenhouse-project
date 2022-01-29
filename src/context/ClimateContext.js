e85ff8a6845256df76f79914eaf03a1632ea6f5c
fiosman <fares.osman@outlook.com>
2022-01-29 00:54:20 -0500

    Wrap app with climate provider

--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -1,7 +1,7 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
 
-import React, { useContext, createContext } from "react";
+import React, { useContext, createContext, useState } from "react";
 
 export const ClimateContext = createContext();
 
